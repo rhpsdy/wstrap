fd748143f4d254eefd395de2a78f3156f960029e
Teddy <heartoftexas@users.noreply.github.com>
2017-02-06 14:36:13 -0600

    Create index.php
    Creation of index.php and barebones imports.

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<!-- WStrap Theme v1.0.0 -->
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
